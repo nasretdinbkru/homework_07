4a6a28c7542ec4253ff0a4a8527c46a52bdeac77
andreiroudenko <andreiroudenko@mail.ru>
2024-11-30 12:23:54 +0300

    Add gtest

--- tests/test_containers.cpp
+++ tests/test_containers.cpp
@@ -4,9 +4,25 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-  std::cout << "___tests start___" << std::endl;
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+//int main(int argc, char **argv)
+//{
+//  std::cout << "___tests start___" << std::endl;
+//  ::testing::InitGoogleTest(&argc, argv);
+//  return RUN_ALL_TESTS();
+//}
+
+
+#include <string>
+using std::string;
+
+const char *actualValTrue  = "hello gtest";
+const char *actualValFalse = "hello world";
+const char *expectVal      = "hello gtest";
+
+TEST(StrCompare, CStrEqual) {
+  EXPECT_STREQ(expectVal, actualValTrue);
+}
+
+TEST(StrCompare, CStrNotEqual) {
+  EXPECT_STREQ(expectVal, actualValFalse);
 }
