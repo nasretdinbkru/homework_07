0c75651ac9ada489e77e06d8b2693cf662ea1637
andreiroudenko <andreiroudenko@mail.ru>
2024-12-01 13:40:55 +0300

    ready

--- tests/test_containers.cpp
+++ tests/test_containers.cpp
@@ -8,7 +8,7 @@
 #include <typeinfo>
 
 #include <string>
-
+// создание контейнера
 TEST(LinkedListTestSuit, LinkedListCTOR) {
   LinkedList linked_list = LinkedList<float>();
   const std::string actual_class_name(typeid(linked_list).name());
@@ -16,7 +16,7 @@ TEST(LinkedListTestSuit, LinkedListCTOR) {
   auto result = actual_class_name.find(expected_class_name);
   ASSERT_NE(result, std::string::npos);
 }
-
+// вставка элементов в конец
 TEST(LinkedListTestSuit, LinkedListPushBackSize) {
   LinkedList linked_list = LinkedList<int>();
   const int test_value = 10;
@@ -26,6 +26,7 @@ TEST(LinkedListTestSuit, LinkedListPushBackSize) {
   ASSERT_EQ(expected_result, actual_result);
 }
 
+// вставка элементов в конец
 TEST(LinkedListTestSuit, LinkedListPushBackValue) {
   LinkedList linked_list = LinkedList<int>();
   const int test_value = 10;
@@ -37,6 +38,7 @@ TEST(LinkedListTestSuit, LinkedListPushBackValue) {
   ASSERT_EQ(expected_value, actual_value1);
 }
 
+// вставка элементов в начало
 TEST(LinkedListTestSuit, LinkedListPushFrontValue) {
   LinkedList linked_list = LinkedList<int>();
   const int test_value = 10;
@@ -49,6 +51,7 @@ TEST(LinkedListTestSuit, LinkedListPushFrontValue) {
 
 }
 
+// вставка элементов в середину
 TEST(LinkedListTestSuit, LinkedListInsert) {
   LinkedList linked_list = LinkedList<int>();
   linked_list.pushBack(1);
@@ -62,6 +65,7 @@ TEST(LinkedListTestSuit, LinkedListInsert) {
   ASSERT_EQ(expected_value, actual_value1) << linked_list[0]->data << linked_list[1]->data << linked_list[2]->data;;
 }
 
+// удаление элементов из конца
 TEST(LinkedListTestSuit, LinkedListPopBack) {
   LinkedList linked_list = LinkedList<int>();
   linked_list.pushBack(1);
@@ -74,6 +78,7 @@ TEST(LinkedListTestSuit, LinkedListPopBack) {
   }
 }
 
+// удаление элементов из начала
 TEST(LinkedListTestSuit, LinkedListPopFront) {
   LinkedList linked_list = LinkedList<int>();
   linked_list.pushBack(1);
@@ -84,6 +89,7 @@ TEST(LinkedListTestSuit, LinkedListPopFront) {
   ASSERT_EQ(expected_value, actual_value);
 }
 
+// удаление элементов из середины
 TEST(LinkedListTestSuit, LinkedListErase) {
   LinkedList linked_list = LinkedList<int>();
   linked_list.pushBack(1);
@@ -95,6 +101,7 @@ TEST(LinkedListTestSuit, LinkedListErase) {
   ASSERT_EQ(expected_value, actual_value);
 }
 
+// получение элементов из контейнера
 TEST(LinkedListTestSuit, LinkedListGetAt) {
   LinkedList linked_list = LinkedList<int>();
   linked_list.pushBack(1);
@@ -105,6 +112,7 @@ TEST(LinkedListTestSuit, LinkedListGetAt) {
   ASSERT_EQ(expected_value, actual_value);
 }
 
+// получение размера контейнера (фактическое количество элементов)
 TEST(LinkedListTestSuit, LinkedListSize) {
   LinkedList linked_list = LinkedList<int>();
   linked_list.pushBack(1);
@@ -112,5 +120,121 @@ TEST(LinkedListTestSuit, LinkedListSize) {
   linked_list.pushBack(3);
   int expected_value = 3;
   int actual_value = static_cast<int>(linked_list.size());
+  ASSERT_EQ(expected_value, actual_value);
+}
+
+// создание контейнера
+TEST(VectorTestSuit, VectorCTOR) {
+  Vector vector = Vector<int>();
+  const std::string actual_class_name(typeid(vector).name());
+  const std::string expected_class_name{"Vector"};
+  auto result = actual_class_name.find(expected_class_name);
+  ASSERT_NE(result, std::string::npos);
+}
+
+// вставка элементов в конец
+TEST(VectorTestSuit, VectorPushBackSize) {
+  Vector vector = Vector<int>();
+  const int test_value = 10;
+  vector.pushBack(test_value);
+  const size_t actual_result = vector.Size();
+  const size_t expected_result = 1;
+  ASSERT_EQ(expected_result, actual_result);
+}
+
+// вставка элементов в конец
+TEST(VectorTestSuit, VectorPushBackValue) {
+  Vector vector = Vector<int>();
+  const int test_value = 10;
+  vector.pushBack(test_value);
+  const int expected_value = test_value;
+  auto actual_value = vector[vector.Size() - 1];
+  ASSERT_EQ(expected_value, actual_value);
+  auto actual_value1 = vector[vector.Size() - 1];
+  ASSERT_EQ(expected_value, actual_value1);
+}
+
+// вставка элементов в начало
+TEST(VectorTestSuit, VectorPushFrontValue) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  const int test_value = 10;
+  const int expected_value = test_value;
+  vector.insert(0, test_value);
+  auto actual_value = vector[0];
+  ASSERT_EQ(expected_value, actual_value);
+
+}
+
+// вставка элементов в середину
+TEST(VectorTestSuit, VectorInsert) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  vector.pushBack(3);
+  const int test_value = 2;
+  const int expected_value = test_value;
+  vector.insert(1, test_value);
+  auto actual_value = vector[1];
+  ASSERT_EQ(expected_value, actual_value) << vector[0] << vector[1] << vector[2];
+  auto actual_value1 = vector[1];
+  ASSERT_EQ(expected_value, actual_value1) << vector[0] << vector[1] << vector[2];;
+}
+
+// удаление элементов из конца
+TEST(VectorTestSuit, VectorPopBack) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  vector.pushBack(2);
+  vector.popBack();
+  try {
+	vector[1];
+  } catch (std::out_of_range const& err) {
+	EXPECT_EQ(err.what(), std::string("Out of range"));
+  }
+}
+
+// удаление элементов из начала
+TEST(VectorTestSuit, VectorEraseFront) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  vector.pushBack(2);
+  vector.erase(0);
+  auto expected_value = 2;
+  int actual_value = vector[0];
+  ASSERT_EQ(expected_value, actual_value);
+}
+
+
+// удаление элементов из середины
+TEST(VectorTestSuit, VectorEraseByIndex) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  vector.pushBack(2);
+  vector.pushBack(3);
+  vector.erase(1);
+  auto expected_value = 3;
+  int actual_value = vector[1];
+  ASSERT_EQ(expected_value, actual_value);
+}
+
+// получение элементов из контейнера
+TEST(VectorTestSuit, VectorGetElementByIndex) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  vector.pushBack(2);
+  vector.pushBack(3);
+  int expected_value = 2;
+  int actual_value = vector[1];
+  ASSERT_EQ(expected_value, actual_value);
+}
+
+// получение размера контейнера (фактическое количество элементов)
+TEST(VectorTestSuit, VectorSize) {
+  Vector vector = Vector<int>();
+  vector.pushBack(1);
+  vector.pushBack(2);
+  vector.pushBack(3);
+  int expected_value = 3;
+  int actual_value = vector.Size();
   ASSERT_EQ(expected_value, actual_value);
 }
